8698259c2cd9055c1dbda1c1105f32d47b0d8f4f
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-11-17 07:12:14 +0100

    Add sync requirements.

--- app/src/main/java/com/mounacheikhna/contactssync/MainActivity.java
+++ app/src/main/java/com/mounacheikhna/contactssync/MainActivity.java
@@ -11,14 +11,19 @@ import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.util.Log;
+import timber.log.Timber;
 
 public class MainActivity extends Activity {
 
     public static final String AUTHORITY = ContactsContract.AUTHORITY;
     public static final String ACCOUNT_TYPE = "com.mounacheikhna.contactssync";
     public static final String ACCOUNT = "com.mounacheikhna.contactssync.account";
+    private static final String PREF_SETUP_COMPLETE = "setup_complete";
+    //private static final long SYNC_FREQUENCY = 60 * 60;  // 15 mn (in seconds)
+    private static final long SYNC_FREQUENCY = 60 * 15;  // 1 mn - temp
 
     private Account account;
 
@@ -41,34 +46,53 @@ public class MainActivity extends Activity {
                 @Override
                 public void onChange(boolean selfChange) {
                     super.onChange(selfChange);
+                    Timber.tag("TEST").d("onChange");
                     query();
                 }
             });
     }
 
     public static Account CreateSyncAccount(Context context) {
-        Account newAccount = new Account(ACCOUNT, ACCOUNT_TYPE);
+        Timber.tag("TEST").d("CreateSyncAccount");
+        boolean newAccount = false;
+        boolean setupComplete = PreferenceManager
+            .getDefaultSharedPreferences(context).getBoolean(PREF_SETUP_COMPLETE, false);
+        Account account = new Account(ACCOUNT, ACCOUNT_TYPE);
         AccountManager accountManager = (AccountManager) context.getSystemService(ACCOUNT_SERVICE);
         /*
          * Add the account and account type, no password or user data
          * If successful, return the Account object, otherwise report an error.
          */
-        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
-            /*
-             * If you don't set android:syncable="true" in
-             * in your <provider> element in the manifest,
-             * then call context.setIsSyncable(account, AUTHORITY, 1)
-             * here.
-             */
-        } else {
-            /*
-             * The account exists or some other error occurred. Log this, report it,
-             * or handle it internally.
-             */
+        if (accountManager.addAccountExplicitly(account, null, null)) {
+            ContentResolver.setIsSyncable(account, AUTHORITY, 1);
+            ContentResolver.setSyncAutomatically(account, AUTHORITY, true);
+            ContentResolver.addPeriodicSync(
+                account, AUTHORITY, new Bundle(), SYNC_FREQUENCY);
         }
+
+        if (newAccount || !setupComplete) {
+            triggerRefresh();
+            PreferenceManager.getDefaultSharedPreferences(context).edit()
+                .putBoolean(PREF_SETUP_COMPLETE, true).commit();
+        }
+        return account;
+    }
+
+    public static void triggerRefresh() {
+        Timber.tag("TEST").d("triggerRefresh ");
+        Bundle b = new Bundle();
+        // Disable sync backoff and ignore sync preferences. In other words...perform sync NOW!
+        b.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        b.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        ContentResolver.requestSync(
+            new Account(ACCOUNT, ACCOUNT_TYPE), // Sync account
+            //"com.goblob.contacts.contactssyncadapter",      // Content authority
+            AUTHORITY,      // Content authority
+            b);                                             // Extras
     }
 
     private void query() {
+        Timber.tag("TEST").d("query");
         ContentResolver cr = getContentResolver();
         String[] projection = new String[] {
             ContactsContract.Contacts.DISPLAY_NAME,
@@ -107,6 +131,7 @@ public class MainActivity extends Activity {
     }
 
     private void checkPermissions() {
+        Timber.tag("TEST").d("checkPermissions");
         //TODO: display a message to the user explaining why these permission are needed
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
